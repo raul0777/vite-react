7d6e962abad0d20df3ad6e4230653d2d61ee32ec
raul0777 <raul.gome44@gmail.com>
2022-08-30 16:11:13 -0300

    Rota 404

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { About } from './components/About'
 import { Home } from './components/Home'
 import { Menu } from './components/Menu'
+import { NotFound } from './components/NotFound'
 import { Post } from './components/Post'
 import { Redirect } from './components/Redirect'
 import './styles/global.css'
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path='/posts/:id' element={<Post/>} />
         <Route path='/posts' element={<Post/>} />
         <Route path='/redirect' element={<Redirect/>} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
