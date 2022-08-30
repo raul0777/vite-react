9ddc9bc567377012e8a5b3a0c2397e93945f07d1
raul0777 <raul.gome44@gmail.com>
2022-08-30 15:21:46 -0300

    Criando links

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { About } from './components/About'
 import { Home } from './components/Home'
+import { Menu } from './components/Menu'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Menu />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
